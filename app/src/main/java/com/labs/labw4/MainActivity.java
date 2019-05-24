1def5a88534e6f522d2f11d2c3ff01c7907b516e
MaximYuwko <maxim.yuwko@gmail.com>
2019-05-24 05:52:46 +0300

    Audio selecting added

--- app/src/main/java/com/labs/labw4/MainActivity.java
+++ app/src/main/java/com/labs/labw4/MainActivity.java
@@ -1,12 +1,15 @@
 package com.labs.labw4;
 
 import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String audioLink = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +25,25 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClickButtonAudio(View view){
+        Intent audioPickerIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        audioPickerIntent.setType("audio/*");
+        startActivityForResult(audioPickerIntent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1 && resultCode == RESULT_OK){
+            audioLink = data.getData().toString();
+        }
+        else{
+            audioLink = null;
+        }
         Intent intent = new Intent(this, ActivityAudio.class);
-        intent.putExtra("linkAudio", "android.resource://" + getPackageName() + "/" + R.raw.sound);
+        if(audioLink == null)
+            intent.putExtra("linkAudio", "android.resource://" + getPackageName() + "/" + R.raw.sound);
+        else
+            intent.putExtra("linkAudio", audioLink);
         startActivity(intent);
     }
 }
